3d412ee8a8b536af273641f979e12f2bc3b143c7
PacoReinaCampo <frareicam@gmail.com>
2026-03-21 19:14:59 +0400

    Working in Instruction Set Architecture

--- RISC-V/instructions/to-pdf.sh
+++ RISC-V/instructions/to-pdf.sh
@@ -1,50 +1,66 @@
-asciidoctor-pdf Base/RISC-V-E-Opcodes.adoc \
+asciidoctor-pdf base/rv-e/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Base/RISC-V-I-Opcodes.adoc \
+asciidoctor-pdf base/rv-i/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Base/RISC-V-O-Opcodes.adoc \
+asciidoctor-pdf base/rv-o/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Standard/RISC-V-A-Opcodes.adoc \
+asciidoctor-pdf standard/rv-a/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Standard/RISC-V-C-Opcodes.adoc \
+asciidoctor-pdf standard/rv-c/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Standard/RISC-V-M-Opcodes.adoc \
+asciidoctor-pdf standard/rv-m/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Algebra/RISC-V-V-Opcodes.adoc \
+asciidoctor-pdf specific/Arithmetic/rv-d/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Algebra/RISC-V-W-Opcodes.adoc \
+asciidoctor-pdf specific/Arithmetic/rv-f/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Algebra/RISC-V-Y-Opcodes.adoc \
+asciidoctor-pdf specific/Arithmetic/rv-q/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Arithmetic/RISC-V-D-Opcodes.adoc \
+asciidoctor-pdf specific/Parallelism/rv-b/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Arithmetic/RISC-V-F-Opcodes.adoc \
+asciidoctor-pdf specific/Parallelism/rv-n/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Arithmetic/RISC-V-Q-Opcodes.adoc \
+asciidoctor-pdf specific/Parallelism/rv-p/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Cryptography/RISC-V-K-Opcodes.adoc \
+asciidoctor-pdf specific/Privilege/rv-h/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Parallelism/RISC-V-P-Opcodes.adoc \
+asciidoctor-pdf specific/Privilege/rv-s/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Parallelism/RISC-V-T-Opcodes.adoc \
+asciidoctor-pdf specific/Algebra/rv-v/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Privilege/RISC-V-H-Opcodes.adoc \
+asciidoctor-pdf specific/Algebra/rv-w/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
 
-asciidoctor-pdf Specific/Privilege/RISC-V-S-Opcodes.adoc \
+asciidoctor-pdf specific/Algebra/rv-y/pseudocode-pl.adoc \
   -a pdf-theme=my-theme.yml
+
+asciidoctor-pdf specific/Cryptography/rv-zk/pseudocode-pl.adoc \
+  -a pdf-theme=my-theme.yml
+
+asciidoctor-pdf specific/Cryptography/rv-zvk/pseudocode-pl.adoc \
+  -a pdf-theme=my-theme.yml
+
+asciidoctor-pdf specific/Cryptography/rv-zwk/pseudocode-pl.adoc \
+  -a pdf-theme=my-theme.yml
+
+asciidoctor-pdf specific/Cryptography/rv-zyk/pseudocode-pl.adoc \
+  -a pdf-theme=my-theme.yml
+
+asciidoctor-pdf specific/Parallelism/rv-t/pseudocode-pl.adoc \
+  -a pdf-theme=my-theme.yml
+
